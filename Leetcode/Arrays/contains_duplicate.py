3f8e9a1de819bcf05b76c6e3f74f08d3dcd5aab8
IamLam163 <olamidealiyu@gmail.com>
2023-07-27 21:38:13 +0100

    alternate approach

--- Leetcode/Arrays/contains_duplicate.py
+++ Leetcode/Arrays/contains_duplicate.py
@@ -11,6 +11,17 @@ class Solution:
         return False
 '''
 
+'''
+Approach 1
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums) - 1):
+                if nums[i] == nums[i + 1]:
+                    return True
+        return False
+'''
+
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
