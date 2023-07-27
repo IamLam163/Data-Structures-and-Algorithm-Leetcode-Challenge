410db3af59a4522d9db755f542d7873781cbe891
IamLam163 <olamidealiyu@gmail.com>
2023-07-27 20:55:07 +0100

    fatal: not a git repository (or any of the parent directories): .git

---
+++ Leetcode/Arrays/contains_duplicate.py
@@ -0,0 +1,57 @@
+from typing import List
+
+'''
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        newhash = {}
+        for i in range(len(nums)):
+            if i in newhash:
+                return True
+            newhash[i] = True
+        return False
+'''
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        visited = {}  # Dictionary to store visited elements
+        for num in nums:
+            if num in visited:
+                return True
+            visited[num] = True
+        return False
+
+
+# Test cases
+test_cases = [
+    # Test case with duplicates
+    ([1, 2, 3, 4, 5, 2], True),
+
+    # Test case without duplicates
+    ([1, 2, 3, 4, 5], False),
+
+    # Test case with negative numbers
+    ([1, -2, 3, -4, 5, -2], True),
+
+    # Test case with empty list
+    ([], False),
+
+    # Test case with large input
+    ([i for i in range(1, 10001)] + [10000], True),
+
+    # Test case with all duplicates
+    ([5] * 10, True),
+
+    # Test case with floating-point numbers
+    ([1.5, 2.0, 3.5, 1.5], True),
+]
+
+# Create an instance of the Solution class
+sol = Solution()
+
+# Perform the tests
+for nums, expected_result in test_cases:
+    result = sol.containsDuplicate(nums)
+    assert result == expected_result, f"Test failed! Input: {nums}, Expected: {expected_result}, Got: {result}"
+
+print("All test cases passed successfully!")
---
+++ practice/hash.py
@@ -0,0 +1,17 @@
+# hash_distribution
+from collections import Counter
+
+
+def distribute(items, num_containers, hash_function=hash):
+    return Counter([hash_function(item) % num_containers for item in items])
+
+
+def plot(histogram):
+    for key in sorted(histogram):
+        count = histogram[key]
+        padding = (max(histogram.values()) - count) * " "
+        print(f"{key:3} {'■' * count}{padding} ({count})")
+
+
+histogram = [5, 6, 6, 3, 9]
+plot(histogram)
